f0a02bb7492683a1314e60852669ef7ea6620326
Paul <paul.bartholomew08@imperial.ac.uk>
2017-10-05 16:19:46 +0100

    Adding plot.py
    This plots the kinetic energy stored in KINETIC.dat which is made by:
    
    grep KINETIC OUTPUT.log > KINETIC.dat

---
+++ plot.py
@@ -0,0 +1,27 @@
+"""
+.. module:: plot.py
+    :synopsis: Plots the kinetic energy in file KINETIC.dat
+.. moduleauthor:: Paul Bartholomew
+"""
+
+import matplotlib.pyplot as plt
+
+def main():
+	"""Main function, does the plotting."""
+
+	t = []
+	k = []
+	with open("KINETIC.dat", "r") as data:
+		for row in data:
+			words = row.split()
+			t.append(float(words[2]))
+			k.append(float(words[3]))
+
+	plt.plot(t, k)
+	plt.xlabel("t")
+	plt.ylabel("k")
+	plt.xlim(xmax=max(t))
+	plt.show()
+
+if __name__ == "__main__":
+	main()
