0d5d97ae2219289065c81725689cf227962d5adb
Paul <paul.bartholomew08@imperial.ac.uk>
2017-10-12 15:49:16 +0100

    Adding PHI and RHO to output of postproc

--- postproc.sh
+++ postproc.sh
@@ -3,6 +3,10 @@
 echo "Updating .dat files"
 grep ENSTROPHY OUTPUT.log > ENSTROPHY.dat
 grep KINETIC OUTPUT.log > KINETIC.dat
+grep "PHI min" OUTPUT.log > PHIMIN.dat
+grep "PHI max" OUTPUT.log > PHIMAX.dat
+grep "RHO min" OUTPUT.log > RHOMIN.dat
+grep "RHO max" OUTPUT.log > RHOMAX.dat
 
 echo "Plotting..."
 python plot.py
